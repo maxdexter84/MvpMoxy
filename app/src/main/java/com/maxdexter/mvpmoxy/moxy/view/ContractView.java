801359d48b6725da445ed8030ac142bb9a57f7df
maxdexter <maxdexter@bk.ru>
2020-06-25 16:34:49 +0300

    скопировал файлы mvp реализации в папку moxy

---
+++ app/src/main/java/com/maxdexter/mvpmoxy/moxy/view/ContractView.java
@@ -0,0 +1,6 @@
+package com.maxdexter.mvpmoxy.moxy.view;
+
+public interface ContractView {
+    String getText();
+    void setText(String text);
+}
---
+++ app/src/main/java/com/maxdexter/mvpmoxy/moxy/view/MainActivity.java
@@ -0,0 +1,54 @@
+package com.maxdexter.mvpmoxy.moxy.view;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.maxdexter.mvpmoxy.R;
+import com.maxdexter.mvpmoxy.moxy.presenter.MainActivityPresenter;
+
+
+public class MainActivity extends AppCompatActivity implements ContractView {
+
+    EditText mEditText;
+    Button mButton;
+    TextView mTextView;
+    MainActivityPresenter mPresenter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        mPresenter = new MainActivityPresenter(this);
+        mButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mPresenter.clickButton();
+                mEditText.getText().clear();
+            }
+        });
+    }
+
+    private void initView() {
+        mEditText = findViewById(R.id.edit_text);
+        mButton = findViewById(R.id.set_text_button);
+        mTextView = findViewById(R.id.textView);
+    }
+
+    @Override
+    public String getText() {
+       String newText =  mEditText.getText().toString();
+       return newText;
+    }
+
+
+
+    @Override
+    public void setText(String text) {
+        mTextView.setText(text);
+    }
+}
