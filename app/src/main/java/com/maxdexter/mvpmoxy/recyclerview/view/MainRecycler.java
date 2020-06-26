eab83ff5fcab21415fd9896d747a4f007f7c90d8
maxdexter <maxdexter@bk.ru>
2020-06-26 13:37:21 +0300

    добавил класс MainRecycler , и новый лайоут item_card_view_image.xml

---
+++ app/src/main/java/com/maxdexter/mvpmoxy/recyclerview/view/MainRecycler.java
@@ -0,0 +1,16 @@
+package com.maxdexter.mvpmoxy.recyclerview.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.maxdexter.mvpmoxy.R;
+
+public class MainRecycler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+    }
+}
