5de710267583ebe9c17f26ca03ddb52be443653f
maxdexter <maxdexter@bk.ru>
2020-06-30 21:28:57 +0300

    создал класс RxActivity.java

---
+++ app/src/main/java/com/maxdexter/mvpmoxy/rx/rxjava/RxActivity.java
@@ -0,0 +1,16 @@
+package com.maxdexter.mvpmoxy.rx.rxjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.maxdexter.mvpmoxy.R;
+
+public class RxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx);
+    }
+}
