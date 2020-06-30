f89bc7f135c51738de9975f41f3edd436383cc66
maxdexter <maxdexter@bk.ru>
2020-06-30 15:35:01 +0300

    создал AsyncActivity

---
+++ app/src/main/java/com/maxdexter/mvpmoxy/rx/async/AsyncActivity.java
@@ -0,0 +1,16 @@
+package com.maxdexter.mvpmoxy.rx.async;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.maxdexter.mvpmoxy.R;
+
+public class AsyncActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_async);
+    }
+}
