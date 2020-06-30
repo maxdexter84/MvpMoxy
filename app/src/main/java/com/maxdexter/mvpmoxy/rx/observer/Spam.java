daeded19120c3847ffc4d267246ef21c3bc212dc
maxdexter <maxdexter@bk.ru>
2020-06-30 17:07:05 +0300

    имплементировал в классе Spam нитерфейс Observable

--- app/src/main/java/com/maxdexter/mvpmoxy/rx/observer/Spam.java
+++ app/src/main/java/com/maxdexter/mvpmoxy/rx/observer/Spam.java
@@ -1,4 +1,21 @@
 package com.maxdexter.mvpmoxy.rx.observer;
 
-public class Spam {
+import java.util.Observer;
+
+public class Spam implements Observable{
+
+    @Override
+    public void registerObserver(Observer observer) {
+        
+    }
+
+    @Override
+    public void unregisterObserver(Observer observer) {
+
+    }
+
+    @Override
+    public void spam() {
+
+    }
 }
