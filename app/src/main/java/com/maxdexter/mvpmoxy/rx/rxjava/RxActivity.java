3f2504c83d9e5f6daf76554697b4631c49898e5f
maxdexter <maxdexter@bk.ru>
2020-07-01 06:43:47 +0300

    добавилв RxActivity.java Observable и Observer

--- app/src/main/java/com/maxdexter/mvpmoxy/rx/rxjava/RxActivity.java
+++ app/src/main/java/com/maxdexter/mvpmoxy/rx/rxjava/RxActivity.java
@@ -3,14 +3,69 @@ package com.maxdexter.mvpmoxy.rx.rxjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.maxdexter.mvpmoxy.R;
 
-public class RxActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+import io.reactivex.*;
+import io.reactivex.disposables.Disposable;
+
+public class RxActivity extends AppCompatActivity {
+    private static final String TAG = "tag";
+    Button subscribeRx;
+    Button unsubscribeRx;
+    Observable<String> observable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx);
+        subscribeRx = findViewById(R.id.subscribeRx);
+        unsubscribeRx = findViewById(R.id.unsubscribeRx);
+        observable = Observable.create(emitter -> {
+            try{
+                List<String> spamList = new ArrayList<>();
+                spamList.add("Spam 1");
+                spamList.add("Spam 2");
+                spamList.add("Spam 3");
+                for (String str:spamList) {
+                    emitter.onNext(str);
+                }
+                emitter.onComplete();
+            }catch (Exception e){
+                emitter.onError(e);
+            }
+
+        });
+        subscribeRx.setOnClickListener(v -> {
+            Observer<String> observer = new Observer<String>() {
+                @Override
+                public void onSubscribe(Disposable d) {
+                    
+                }
+                @Override
+                public void onNext(String s) {
+                    Log.d(TAG,s);
+                }
+
+                @Override
+                public void onError(Throwable e) {
+                    Log.d(TAG,e.getMessage());
+                }
+
+                @Override
+                public void onComplete() {
+
+                }
+            };
+            observable.subscribe(observer);
+
+        });
+
+
     }
 }
