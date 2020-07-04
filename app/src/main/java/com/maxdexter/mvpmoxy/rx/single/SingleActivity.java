f2e7dfc631322008b62cefb4970fc063a5b8038a
maxdexter <maxdexter@bk.ru>
2020-07-04 18:30:38 +0300

    добавил activity_single.xml

--- app/src/main/java/com/maxdexter/mvpmoxy/rx/single/SingleActivity.java
+++ app/src/main/java/com/maxdexter/mvpmoxy/rx/single/SingleActivity.java
@@ -1,4 +1,17 @@
 package com.maxdexter.mvpmoxy.rx.single;
 
-public class SingleActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.maxdexter.mvpmoxy.R;
+
+public class SingleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single);
+
+    }
 }
