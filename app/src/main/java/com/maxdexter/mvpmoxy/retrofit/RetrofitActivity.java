6d92b6cd67d6eb350e2e2ab2c33a7e5b86cf0297
maxdexter <maxdexter@bk.ru>
2020-07-24 15:05:06 +0300

    создал RetrofitActivity

---
+++ app/src/main/java/com/maxdexter/mvpmoxy/retrofit/RetrofitActivity.java
@@ -0,0 +1,16 @@
+package com.maxdexter.mvpmoxy.retrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.maxdexter.mvpmoxy.R;
+
+public class RetrofitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrofit);
+    }
+}
