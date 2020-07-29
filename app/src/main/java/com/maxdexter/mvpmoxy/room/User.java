45335387f3119010fd9640abf78743bde533bc4b
maxdexter <maxdexter@bk.ru>
2020-07-29 11:29:49 +0300

    добавил RecyclerAdapter

--- app/src/main/java/com/maxdexter/mvpmoxy/room/User.java
+++ app/src/main/java/com/maxdexter/mvpmoxy/room/User.java
@@ -14,4 +14,28 @@ public class User {
     private String surname;
     @ColumnInfo(name = "age")
     private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
---
+++ app/src/main/java/com/maxdexter/mvpmoxy/room/database/adapter/RecyclerAdapter.java
@@ -0,0 +1,57 @@
+package com.maxdexter.mvpmoxy.room.database.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.maxdexter.mvpmoxy.R;
+import com.maxdexter.mvpmoxy.room.User;
+
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    List<User> users;
+    public RecyclerAdapter (List<User> users) {
+        this.users = users;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        User user = users.get(position);
+        holder.bind(user);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView userName;
+        TextView userSurname;
+        TextView userAge;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            userName = itemView.findViewById(R.id.tv_user_name);
+            userSurname = itemView.findViewById(R.id.tv_user_surname);
+            userAge = itemView.findViewById(R.id.tv_user_age);
+        }
+        private void bind(User user) {
+            userName.setText(user.getName());
+            userSurname.setText(user.getSurname());
+            userSurname.setText(user.getAge());
+        }
+    }
+}
