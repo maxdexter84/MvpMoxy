d228a99ff5700a2712acd3958cf3d83e6280f047
maxdexter <maxdexter@bk.ru>
2020-07-30 10:20:05 +0300

    переделал классы Green and White  в соответствии со вторым заданием

--- app/src/main/java/com/maxdexter/mvpmoxy/di/diActivity/Red.java
+++ app/src/main/java/com/maxdexter/mvpmoxy/di/diActivity/Red.java
@@ -4,9 +4,10 @@ import android.util.Log;
 
 public class Red {
     private static final String TAG = "tag";
+    Green mGreen;
 
-    public Red() {
-        Green green = new Green();
-        Log.d(TAG,green.getGreen() + " " + green.toString());
+    public Red(Green green) {
+        this.mGreen = green;
+        Log.d(TAG,mGreen.getGreen() + " " + mGreen.toString());
     }
 }
--- app/src/main/java/com/maxdexter/mvpmoxy/di/diActivity/White.java
+++ app/src/main/java/com/maxdexter/mvpmoxy/di/diActivity/White.java
@@ -5,9 +5,10 @@ import android.util.Log;
 public class White {
 
     private static final String TAG = "tag";
+    Green mGreen;
 
-    public White() {
-        Green green = new Green();
-        Log.d(TAG,green.getGreen() + " " + green.toString());
+    public White(Green green) {
+        this.mGreen = green;
+        Log.d(TAG,mGreen.getGreen() + " " + mGreen.toString());
     }
 }
