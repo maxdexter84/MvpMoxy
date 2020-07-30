57a8ab0858cc4c60d5aeb7e04abe797252c3f815
maxdexter <maxdexter@bk.ru>
2020-07-30 15:21:40 +0300

    создал интерфейс AppComponent и класс AppModule

---
+++ app/src/main/java/com/maxdexter/mvpmoxy/di/dagger/app/AppComponent.java
@@ -0,0 +1,17 @@
+package com.maxdexter.mvpmoxy.di.dagger.app;
+
+import com.maxdexter.mvpmoxy.di.dagger.DaggerActivity;
+import com.maxdexter.mvpmoxy.di.dagger.Red;
+import com.maxdexter.mvpmoxy.di.dagger.White;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+@Singleton
+@Component
+public interface AppComponent {
+   void inject(DaggerActivity daggerActivity);
+   void inject(Red red);
+   void inject(White white);
+}
---
+++ app/src/main/java/com/maxdexter/mvpmoxy/di/dagger/app/AppModule.java
@@ -0,0 +1,30 @@
+package com.maxdexter.mvpmoxy.di.dagger.app;
+
+import android.app.Application;
+
+import com.maxdexter.mvpmoxy.di.dagger.Green;
+import com.maxdexter.mvpmoxy.di.dagger.Red;
+import com.maxdexter.mvpmoxy.di.dagger.White;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class AppModule {
+
+    private final Application mApplication;
+
+    public AppModule(Application application) {
+        mApplication = application;
+    }
+
+
+    @Singleton
+    @Provides
+    Green provideRed() {
+        return new Green();
+    }
+
+}
