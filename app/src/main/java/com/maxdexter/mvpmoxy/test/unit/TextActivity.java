5226ced3c875139d679eb174995f53755369d716
maxdexter <maxdexter@bk.ru>
2020-08-04 15:26:55 +0300

    изменил TextActivity.java

---
+++ app/src/main/java/com/maxdexter/mvpmoxy/test/unit/TextActivity.java
@@ -0,0 +1,33 @@
+package com.maxdexter.mvpmoxy.test.unit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.maxdexter.mvpmoxy.R;
+
+public class TextActivity extends AppCompatActivity {
+    Button mButton;
+    TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text);
+
+        mButton = findViewById(R.id.text_button);
+        mTextView = findViewById(R.id.tv_text_activity);
+
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextView.setText("TextView");
+            }
+        });
+    }
+
+
+}
