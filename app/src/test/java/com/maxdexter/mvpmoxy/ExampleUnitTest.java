1941d4f0bbd9cf5b5f9f9c570e91a8d8ff516bad
maxdexter <maxdexter@bk.ru>
2020-08-04 14:53:59 +0300

    добавил два unit testa

--- app/src/test/java/com/maxdexter/mvpmoxy/ExampleUnitTest.java
+++ app/src/test/java/com/maxdexter/mvpmoxy/ExampleUnitTest.java
@@ -1,26 +1,31 @@
 package com.maxdexter.mvpmoxy;
 
+import com.maxdexter.mvpmoxy.test.unit.Math;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
+    private Math mMath;
     @Before
+    public void before() {
+         mMath = new Math();
+    }
     @Test
     public void addition_isCorrect() {
-        int res =
+        int a = 2;
+        int b = 2;
+        int res = mMath.addition(a, b);
         Assert.assertEquals(4, 2 + 2);
     }
     @Test
     public void addition_isIncorrect() {
-        Assert.assertNotEquals(4, 2 + 3);
+        int a = 2;
+        int b = 2;
+        int res = mMath.addition(a, b);
+        Assert.assertNotEquals(5, 2 + 2);
     }
 
 }
